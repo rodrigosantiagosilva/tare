584795451ca8758f05242bd8acfd4e92a6c955fe
Rodrigo <rodrigosantiagosilva86@gmail.com>
2025-04-30 15:34:26 -0300

    AAAAAAAAAAAA

--- index.php
+++ index.php
@@ -26,107 +26,34 @@ $errorMiddleware->setErrorHandler(HttpNotFoundException::class, function (
     return $response->withHeader('Content-Type', 'application/json')->withStatus(404);
 });
 
-$app->get('/teste-debug',function (Request $request, Response $response, array $args){
-    $debug = new Debug();
-    $response->getBody()->write($debug->debug('teste 00001'));
-    return $response;
-});
 
-
-
-$app->get("/math/soma/{num1}/{num2}", function (Request $request, Response $response, array $args){
+$app->get("/math/areaquadrado/{num1}/{num2}", function (Request $request, Response $response, array $args){
     $math = new Math();
-    $resultado = $math->soma($args['num1'],$args['num2']);
+    $resultado = $math->areaquadrado($args['num1'],$args['num2']);
     $response->getBody()->write((string) $resultado);
     return $response;
 });
 
-$app->get("/math/menos/{num1}/{num2}", function (Request $request, Response $response, array $args){
+$app->get("/math/areatriangulo/{num1}/{num2}", function (Request $request, Response $response, array $args){
     $math = new Math();
-    $resultado = $math->menos($args['num1'],$args['num2']);
+    $resultado = $math->areatriangulo($args['num1'],$args['num2']);
     $response->getBody()->write((string) $resultado);
     return $response;
 });
 
-$app->get("/math/produto/{num1}/{num2}", function (Request $request, Response $response, array $args){
+$app->post("/math/areatriangulo/{num1}/{num2}", function (Request $request, Response $response, array $args){
     $math = new Math();
-    $resultado = $math->produto($args['num1'],$args['num2']);
+    $resultado = $math->areatriangulo($args['num1'],$args['num2']);
     $response->getBody()->write((string) $resultado);
     return $response;
 });
-
-$app->get("/math/divide/{num1}/{num2}", function (Request $request, Response $response, array $args){
+$app->post("/math/areaquadrado/{num1}/{num2}", function (Request $request, Response $response, array $args){
     $math = new Math();
-    $resultado = $math->divide($args['num1'],$args['num2']);
+    $resultado = $math->areaquadrado($args['num1'],$args['num2']);
     $response->getBody()->write((string) $resultado);
     return $response;
 });
 
 
-$app->get("/math/quadrado/{num1}", function (Request $request, Response $response, array $args){
-    $math = new Math();
-    $resultado = $math->quadrado($args['num1']);
-    $response->getBody()->write((string) $resultado);
-    return $response;
-});
-$app->get("/math/raiz/{num1}", function (Request $request, Response $response, array $args){
-    $math = new Math();
-    $resultado = $math->raiz($args['num1']);
-    $response->getBody()->write((string) $resultado);
-    return $response;
-});
-
-
-
-$app->get('/tarefas', function (Request $request, Response $response, array $args) {
-    $tarefa_service = new TarefaService();
-    $tarefas = $tarefa_service -> getAllTarefas();
-
-    $response->getBody()->write(json_encode($tarefas));
-    return $response->withHeader('Content-Type','application/json');
-});
-
-
-$app->post('/tarefas', function (Request $request, Response $response, array $args) {
-    $parametros =(array) $request ->getParsedBody();
-
-    if(!array_key_exists('titulo',$parametros) || empty($parametros['titulo'])){
-        $response->getBody()->write(json_encode([
-            "mensagem" => "titulo obrigatorio"
-        ]));
-    }
-    $modelo = array_merge(['titulo' =>'','concluido' =>false],$parametros);
-    $tarefa_service = new TarefaService();
-    $tarefa_service ->createTarefa($parametros);
-    return $response->withStatus(201);
-});
-
-
-
-$app->delete('/tarefas/{id}', function (Request $request, Response $response, array $args) {
-    $id = $args['id'];
-    $tarefa_service = new TarefaService();
-    $tarefa_service->deleteTarefa($id);
-    return $response->withStatus(202);
-});
-
-$app->put('/tarefas/{id}', function (Request $request, Response $response, array $args) {
-    $id = $args['id'];
-    $dados_para_atualizar = json_decode($request ->getBody()->getContents(),true);
-    if(array_key_exists('titulo',$dados_para_atualizar) && empty($dados_para_atualizar['titulo'])){
-        $response->getBody()->write(json_encode([
-            "mensagem" =>"titulo é obrigatorio"
-
-
-        ]));
-        return $response->withHeader('Content-Type','application/json')->withStatus(400);
-    }
-    $tarefa_service = new TarefaService();
-    $tarefa_service->updateTarefa($id,$dados_para_atualizar);
-
-    return $response->withStatus(201);
-});
- 
- 
  
 $app->run();
